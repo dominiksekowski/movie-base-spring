4cb9bf2db3c392902e408f0ac244be3d883f4f2d
dominiksekowski <dominiksekowski.chat@wp.pl>
2020-07-20 19:09:39 +0200

    Base structure

---
+++ src/main/java/com/domseko/moviebasespring/MovieBaseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.domseko.moviebasespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBaseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domseko/moviebasespring/MovieBaseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domseko.moviebasespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBaseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
